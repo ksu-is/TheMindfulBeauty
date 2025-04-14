4e6163b45063f20d2266ac386533a1842149b6ee
Victoria Dablah <vdablah@students.kennesaw.edu>
2025-04-14 19:22:13 -0400

    Created dictionary, user input, and output for search query
    Created a dictionary to store paired data, allowed user input, and created desired output . More to come!

--- BeautifulSoup.py
+++ BeautifulSoup.py
@@ -8,15 +8,36 @@ soup = BeautifulSoup(page.text, 'html.parser')
 results = soup.find(id="rating__rows")
 data= results.find_all("div", class_="rating__row") 
 
+brands_data = []
+
 for info in data: 
     brand_name = info.find("h3", class_="text-xs")
     ethics = info.find_all("h6", class_="text-base") #replaced find with find_all so it returns all matches as a list
     rating = info.find("div", class_="rating__index")
 
-    if brand_name and ethics and rating:
-        print(brand_name.text.strip())
-        #added necessary loop
-        for diff in ethics:
-            print(diff.text.strip())
-        print(rating.text.strip())
+    print(ethics)
+
+    if brand_name and ethics and rating: #ensures they are not empty
+        brands_data.append({     #dictionary created
+            'name': brand_name.text.strip(),
+            'ethics': [criteria.text.strip() for criteria in ethics], #ensures all criteria for each company is collected 
+            'rating': rating.text.strip()
+        })
+
+
+user_input = input("Welcome! Enter a brand name: ").strip().lower() #allows user to search for specific brands 
+
+available = False
+for brand in brands_data:
+    if brand['name'].lower() == user_input:
+        print("\nBrand: " + brand['name'])
+        print("Ethical Criteria:")
+        for e in brand['ethics']:
+            print(" - " + e)
+        print("Overall Rating: " + brand['rating'])
+        found = True
+        break
+
+if not available:
+    print("Brand not found. Please check the spelling or try another brand!")
 
