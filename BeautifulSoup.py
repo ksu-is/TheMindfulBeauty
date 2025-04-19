f971aa6b11b491656dc09a614faa12baa1c7269c
Victoria Dablah <vdablah@students.kennesaw.edu>
2025-04-19 18:59:08 -0400

    Added column and utilized CSS selectors
    Fixed errors, used CSS selectors to more accurately scrape data, and added Parent Company column

--- BeautifulSoup.py
+++ BeautifulSoup.py
@@ -4,36 +4,35 @@ import pandas as pd
 
 brands_data = []
 
-#since only one URL neccesary took away 'for criteria:' portion
 url = 'https://thegoodshoppingguide.com/subject/ethical-skincare/'
 response = requests.get(url)
 soup = BeautifulSoup(response.text, 'html.parser')
 
-results = soup.find(id="rating__rows")
-table = results.find_all("div", class_="rating__row") 
+results = soup.select("#rating__rows .rating__row") #broadened my reach to get all info needed
+for data in results: #switched to using a CSS selector to scrape mroe accurately
+    brand_tag = data.select_one("h4")
+    parent_company_tag = data.select_one("p.text-base")
+    GSG_score_tag = data.select_one("div.rating__index")
 
-for info in table:
-    brand_info = info.find("h3", class_="text-xs")
-    ethics_info = info.find_all("h6", class_="text-base") #replaced find with find_all so it returns all matches as a list
-    rating_info = info.find_all("span", class_="sr-only")
-    score_info = info.find("div", class_="rating__index")
+    brand = brand_tag.text.strip().replace(" Ethical Rating", "") #got rid of unecessary text
+    parent_company = parent_company_tag.text.strip().replace("Parent company: ", "") #got rid of unecessary text
+    GSG_score = GSG_score_tag.text.strip()
 
-    if brand_info and ethics_info and rating_info:
-        csv_data = {
-            'name': brand_info.text.strip(),
-            'score': score_info.text.strip()
+    csv_data = {
+            'Brand': brand,
+            'Parent Company': parent_company,
+            'GSG Score': GSG_score
         }
-        
-        valid_ratings = ["Good", "Poor", "Acceptable"]
-        for i in range(len(ethics_info)):
-            ethic = ethics_info[i].text.strip() 
-            rating = rating_info[i].text.strip()
 
-            if rating in valid_ratings:
-                csv_data[ethic] = rating #makes each ethic its own column with its correpsonding rating 
-            
-        brands_data.append(csv_data)
+    for pair in data.select("li"):  #changed to li elements to more accurately scrape
+        criteria = pair.select_one("h6")
+        rating = pair.select_one("span.sr-only")
+        if criteria and rating:
+            csv_data[criteria.text.strip()] = rating.text.strip() #makes each ethic its own column with its correpsonding rating 
 
-page = pd.DataFrame(brands_data)
-page.to_csv('skincare.csv', index=False, encoding='utf-8-sig') #added 'utf-8-sig' parameter to fix misreading of csv file 
+    brands_data.append(csv_data)
+
+scraped = pd.DataFrame(brands_data)
+scraped.to_csv('skincare.csv', index=False, encoding='utf-8-sig') #added 'utf-8-sig' parameter to fix misreading of csv file 
 print('skincare.csv created!')
+print(scraped)
